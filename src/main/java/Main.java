344d26e487dc1811a90c70549171397b0b5fb971
nickl0gist <nicklogistick@mail.ru>
2019-06-16 13:58:39 +0200

    Main method updated to print every separate sms.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,6 @@
 import java.math.BigDecimal;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,12 +10,18 @@ public class Main {
         CostCalculator costCalculator = new CostCalculator(new BigDecimal(args[2]));
 
         String compressedMessage = nativeCompressor.compress(args[0]);
-        int messageQty = paginator.paginate(compressedMessage).length;
-        BigDecimal cost = costCalculator.calculate(messageQty);
+        String[] messages = paginator.paginate(compressedMessage);
+        BigDecimal cost = costCalculator.calculate(messages.length);
 
-        System.out.println("Your message after compression: \n" + compressedMessage);
-        System.out.printf("It will be splitted to %s sms \n", messageQty);
-        System.out.println("The total cost of all sms is " + cost);
+        StringBuilder sb = new StringBuilder();
+        sb.append("Your message after compression: \n" + compressedMessage);
+        sb.append(String.format("It will be splited to %s sms: \n", messages.length));
+        for (String s: messages) {
+            sb.append(s+"\n");
+        }
+        sb.append("The total cost of all sms is " + cost);
+
+        System.out.println(sb.toString());
 
 
     }
