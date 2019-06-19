72e5338fa5cb10ef291af321778330181edb8cc9
nickl0gist <nicklogistick@mail.ru>
2019-06-19 12:28:14 +0200

    CLI realized with usage of picocli

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,27 +1,52 @@
 import java.math.BigDecimal;
 
-public class Main{
+import picocli.CommandLine;
+import picocli.CommandLine.*;
+import picocli.CommandLine.Parameters;
+
+@Command(description = "Compressing test to short messages using Native and Advanced methods",
+        name = "sms_compress", mixinStandardHelpOptions = true)
+
+public class Main implements Runnable{
+
+    @Option(names = { "-m", "--message" }, description = "Text message [Please use \"\"]", required = true)
+    private static String message;
+
+    @Option(names = { "-l", "--length" }, description = "Length of single text message [number of characters] ",
+            required = true)
+    private static int length;
+
+    @Option(names = { "-p", "--price" }, description = "Price of single text message [PLN]")
+    private static double price;
+
+    @Option(names = "-n", description = "Naive compressor turning on [Please use -n or -o]")
+    private static boolean naiveCompressor;
+
+    @Option(names = "-o", description = "Optimal compressor turning on [Please use -n or -o]")
+    private static boolean optimalCompressor;
 
     public static void main(String[] args) {
+        CommandLine.run(new Main(), args);
+    }
 
-        NativeCompressor nativeCompressor = new NativeCompressor();
-        Paginator paginator = new Paginator(Integer.parseInt(args[1]));
-        CostCalculator costCalculator = new CostCalculator(new BigDecimal(args[2]));
-
-        String compressedMessage = nativeCompressor.compress(args[0]);
-        String[] messages = paginator.paginate(compressedMessage);
-        BigDecimal cost = costCalculator.calculate(messages.length);
+    @Override
+    public void run() {
+        Paginator paginator = new Paginator(length);
+        CostCalculator costCalculator = new CostCalculator(new BigDecimal(price));
+        Compressor compressor = optimalCompressor ?  new AdvancedCompressor(): new NativeCompressor();
+        String compressedMessage = compressor.compress(message);
+        String[] sms_messages = paginator.paginate(compressedMessage);
+        BigDecimal cost = costCalculator.calculate(sms_messages.length);
 
         StringBuilder sb = new StringBuilder();
         sb.append("Your message after compression: \n" + compressedMessage);
-        sb.append(String.format("It will be splited to %s sms: \n", messages.length));
-        for (String s: messages) {
+        sb.append(String.format("It will be splited to %s sms: \n", sms_messages.length));
+        for (String s: sms_messages) {
             sb.append(s+"\n");
         }
-        sb.append("The total cost of all sms is " + cost);
+        sb.append("The total cost of all sms is " + cost + "\n");
+        sb.append("The message after decompression is [" + compressor.deCompress(compressedMessage) + "]");
 
         System.out.println(sb.toString());
-
     }
-
 }
